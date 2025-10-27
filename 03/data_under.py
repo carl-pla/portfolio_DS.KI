250024b62d22bbdde08815c21ea874d2ea4c580c
Carl Placek <carl.placek@icloud.com>
2025-10-27 13:27:53 +0100

    wine_testing aufgaben 03 angefangen

---
+++ 03/data_under.py
@@ -0,0 +1,7 @@
+import pandas as pd 
+import matplotlib.pyplot as plt 
+
+# CSV-Datei für Rotwein einlesen
+df = pd.read_csv('/Users/carlplacek/Desktop/Uni/DataScience&KI/wine+quality/winequality-red.csv', sep=';')
+
+print(df.head(6))
